a211547d9abdb35c4f92bcbfcd9af6079bff7a64
AlexMiroshnikov <ilex@ilex-thinknote>
2015-11-14 12:27:47 +0700

    Initial (grab from parent mufuphlex)

---
+++ src/ArrayUtil.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Mufuphlex\Util;
+
+/**
+ * Class ArrayUtil
+ */
+class ArrayUtil
+{
+    /**
+     * @param array $array
+     * @param array $map
+     * @return array
+     */
+    public static function cutByWhitelist(array &$array, array $map)
+    {
+        foreach ($array as $key => $value) {
+            if (isset($map[$key])) {
+                if (is_array($map[$key]) AND is_array($value)) {
+                    $array[$key] = self::cutByWhitelist($value, $map[$key]);
+                } elseif ($map[$key] instanceof \Closure) {
+                    $array[$key] = $map[$key]($value);
+                }
+            } else {
+                $unset = true;
+                $mapKeys = array_keys($map);
+
+                foreach ($mapKeys as $mapKey) {
+                    if (preg_match('@^/.+/[siu]*$@', $mapKey) AND preg_match($mapKey, $key)) {
+                        if (is_array($map[$mapKey])) {
+                            $array[$key] = self::cutByWhitelist($value, $map[$mapKey]);
+                        } elseif ($map[$mapKey] instanceof \Closure) {
+                            $array[$key] = $map[$mapKey]($value);
+                        }
+                        $unset = false;
+                        break;
+                    }
+                }
+
+                if ($unset) {
+                    unset($array[$key]);
+                }
+            }
+        }
+
+        return $array;
+    }
+
+    /**
+     * @param array $array
+     * @param array $map
+     * @return array
+     */
+    public static function cutByBlacklist(array &$array, array $map)
+    {
+        $mapKeys = array_keys($map);
+
+        foreach ($array as $key => $value) {
+            $unset = false;
+
+            if (isset($map[$key])) {
+                if (is_array($map[$key])) {
+                    $array[$key] = self::cutByBlacklist($value, $map[$key]);
+                } elseif ($map[$key] instanceof \Closure) {
+                    $array[$key] = $map[$key]($value);
+                } else {
+                    $unset = true;
+                }
+            } else {
+                foreach ($mapKeys as $mapKey) {
+                    if (preg_match('@^/.+/[siu]*$@', $mapKey) AND preg_match($mapKey, $key)) {
+                        if (is_array($map[$mapKey])) {
+                            $array[$key] = self::cutByBlacklist($value, $map[$mapKey]);
+                        } elseif ($map[$mapKey] instanceof \Closure) {
+                            $array[$key] = $map[$mapKey]($value);
+                        } else {
+                            $unset = true;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if ($unset) {
+                unset($array[$key]);
+            }
+        }
+
+        return $array;
+    }
+
+    /**
+     * @param array $array
+     * @param bool $keepKeys
+     * @return array
+     */
+    public static function unique(array $array, $keepKeys = false)
+    {
+        if ($keepKeys) {
+            $array = array_reverse($array, true);
+        }
+
+        $flip = array_flip($array);
+
+        if (!$keepKeys) {
+            return array_keys($flip);
+        }
+
+        return array_flip($flip);
+    }
+
+    /**
+     * @param array $array , $array2[, $array3, ...]
+     * @return array
+     */
+    public static function intersect(array $array)
+    {
+        if (($argsCnt = func_num_args()) < 2) {
+            throw new \InvalidArgumentException('At least 2 arrays must be passed');
+        }
+
+        $array2 = func_get_arg(1);
+        $flip = array_flip($array2);
+
+        foreach ($array as $key => $value) {
+            if (!isset($flip[$value])) {
+                unset($array[$key]);
+            }
+        }
+
+        if ($argsCnt > 2) {
+            $args = func_get_args();
+            $args = array_slice($args, 2);
+            array_unshift($args, $array);
+            return call_user_func_array(array(__CLASS__, __FUNCTION__), $args);
+        }
+
+        return $array;
+    }
+}
---
+++ tests/ArrayUtilTest.php
@@ -0,0 +1,403 @@
+<?php
+class ArrayUtilTest extends PHPUnit_Framework_TestCase
+{
+	public function testWhitelistSimpleCase()
+	{
+		$map = array(
+			'key' => array(
+				'value' => array(
+					'low level' => true
+				)
+			)
+		);
+
+		$array = array(
+			'key' => array(
+				'value' => array(
+					'low level' => 123,
+					'another low level' => 456
+				),
+				'another value' => array(
+					'low level' => 789,
+					'another low level' => 'string'
+				)
+			),
+			'another key' => array(
+				'value' => array(
+					'low level' => 1234,
+					'another low level' => 4567
+				),
+				'another value' => array(
+					'low level' => 7890,
+					'another low level' => 'another string'
+				)
+			)
+		);
+
+		$cutByWhitelist = \Mufuphlex\Util\ArrayUtil::cutByWhitelist($array, $map);
+
+		$this->assertEquals(
+			array(
+				'key' => array(
+					'value' => array(
+						'low level' => 123
+					)
+				)
+			),
+			$cutByWhitelist
+		);
+	}
+
+	public function testWhitelistRegexCase()
+	{
+		$map = array(
+			'/^(?:one|another)$/' => array(
+				'/\d+/' => array(
+					'low level' => true
+				)
+			)
+		);
+
+		$array = array(
+			'one another' => array(
+				1 => array(
+					'low level' => 'unexpected due to regex'
+				),
+				'not numeric' => array(
+					'low level' => 'unexpected due to not numeric',
+				),
+				2 => array(
+					'low level' => 'unexpected due to regex as well',
+				),
+			),
+			'one' => array(
+				3 => array(
+					'the first low level' => 'unexpected due to key',
+					'low level' => 'low level in one - 1'
+				),
+				'not numeric' => array(
+					'low level' => 'unexpected due to not numeric',
+					'another low level' => 'unexpected in one'
+				),
+				4 => array(
+					'low level' => 'low level in one - 2',
+					'the second low level' => 'unexpected due to key',
+				)
+			),
+			'another' => array(
+				5 => array(
+					'the first low level' => 'unexpected due to key',
+					'low level' => 'low level in another - 1'
+				),
+				'not numeric' => array(
+					'low level' => 'unexpected due to not numeric',
+					'another low level' => 'unexpected in another'
+				),
+				6 => array(
+					'low level' => 'low level in another - 2',
+					'the second low level' => 'unexpected due to key',
+				)
+			)
+		);
+
+		$cutByWhitelist = \Mufuphlex\Util\ArrayUtil::cutByWhitelist($array, $map);
+
+		$this->assertEquals(
+			array(
+				'one' => array(
+					3 => array(
+						'low level' => 'low level in one - 1'
+					),
+					4 => array(
+						'low level' => 'low level in one - 2'
+					)
+				),
+				'another' => array(
+					5 => array(
+						'low level' => 'low level in another - 1'
+					),
+					6 => array(
+						'low level' => 'low level in another - 2'
+					)
+				)
+			),
+			$cutByWhitelist
+		);
+
+	}
+
+	public function testWhitelistClosureCase()
+	{
+		$map = array(
+			0 => array(
+				'/\d+/' => function($arg){return $arg*2+1;}
+			)
+		);
+
+		$array = array(
+			array(
+				0,
+				1,
+				2,
+				'internal string key' => 'unexpected internal content'
+			),
+			'string key' => 'unexpected content'
+		);
+
+		$cutByWhitelist = \Mufuphlex\Util\ArrayUtil::cutByWhitelist($array, $map);
+
+		$this->assertEquals(
+			array(
+				array(
+					1,
+					3,
+					5
+				)
+			),
+			$cutByWhitelist
+		);
+
+		$map = array(
+			0 => function($arg){return $arg+2;}
+		);
+
+		$array = array(
+			'not numeric' => 1,
+			0 => 2
+		);
+
+		$cutByWhitelist = \Mufuphlex\Util\ArrayUtil::cutByWhitelist($array, $map);
+
+		$this->assertEquals(
+			array(
+				0 => 4
+			),
+			$cutByWhitelist
+		);
+	}
+
+	public function testBlackListSimpleCase()
+	{
+		$map = array(
+			'string' => array(
+				2 => true
+			)
+		);
+
+		$array = array(
+			array(
+				2 => array(
+					'expected'
+				)
+			),
+			'string' => array(
+				'expected',
+				2 => 'unexpected'
+			)
+		);
+
+		$cut = \Mufuphlex\Util\ArrayUtil::cutByBlacklist($array, $map);
+
+		$this->assertEquals(
+			array(
+				array(
+					2 => array(
+						'expected'
+					)
+				),
+				'string' => array(
+					'expected'
+				)
+			),
+			$cut
+		);
+	}
+
+	public function testBlackListRegexCase()
+	{
+		$map = array(
+			'protocol' => array(
+				'/https?/' => true
+			)
+		);
+
+		$array = array(
+			'not protocol' => array(
+				'http' => 'expected',
+				'https' => 'expected too',
+				'ftp' => 'expected as well'
+			),
+			'protocol' => array(
+				'http' => 'unexpected',
+				'https' => 'unexpected too',
+				'ftp' => 'expected'
+			),
+		);
+
+		$cut = \Mufuphlex\Util\ArrayUtil::cutByBlacklist($array, $map);
+
+		$this->assertEquals(
+			array(
+				'not protocol' => array(
+					'http' => 'expected',
+					'https' => 'expected too',
+					'ftp' => 'expected as well'
+				),
+				'protocol' => array(
+					'ftp' => 'expected'
+				)
+			),
+			$cut
+		);
+	}
+
+	public function testBlacklistClosureCase()
+	{
+		$map = array(
+			'/^\d+$/' => array(
+				1 => function($arg){return $arg+5;}
+			)
+		);
+
+		$array = array(
+			'string' => 'expected',
+			array(
+				1,
+				2
+			),
+			array(
+				3,
+				4
+			)
+		);
+
+		$cut = \Mufuphlex\Util\ArrayUtil::cutByBlacklist($array, $map);
+
+		$this->assertEquals(
+			array(
+				'string' => 'expected',
+				array(
+					1,
+					7
+				),
+				array(
+					3,
+					9
+				)
+			),
+			$cut
+		);
+
+		$map = array(
+			'/^\d+$/' => function($arg){return $arg+2;},
+			'not numeric' => true
+		);
+
+		$array = array(
+			'not numeric' => 1,
+			0 => 2
+		);
+
+		$cut = \Mufuphlex\Util\ArrayUtil::cutByBlacklist($array, $map);
+
+		$this->assertEquals(
+			array(
+				0 => 4
+			),
+			$cut
+		);
+	}
+
+	public function testUnique()
+	{
+		$array = array(1,2,3,4,5,1,2,3,1,2,1);
+		$unique = array(1,2,3,4,5);
+		$this->assertEquals($unique, \Mufuphlex\Util\ArrayUtil::unique($array));
+		$array = array('a','b','a');
+		$unique = array('a', 'b');
+		$this->assertEquals($unique, \Mufuphlex\Util\ArrayUtil::unique($array));
+	}
+
+	public function testUniqueKeepingKey()
+	{
+		$array = array(
+			'a' => 1,
+			'b' => 2,
+			'c' => 1,
+			'd' => 2,
+			'e' => 1,
+			'f' => 3
+		);
+
+		$unique = array(
+			'a' => 1,
+			'b' => 2,
+			'f' => 3
+		);
+
+		$this->assertEquals($unique, \Mufuphlex\Util\ArrayUtil::unique($array, true));
+	}
+
+	public function testIntersect()
+	{
+		$a = array(1,2,3);
+		$b = array(3,4,5);
+		$expected = array(2 => 3);
+		$this->assertEquals($expected, \Mufuphlex\Util\ArrayUtil::intersect($a, $b));
+
+		$a = array(1,2,3,4);
+		$b = array(3,4,5,6);
+		$c = array(4,5,6,7);
+		$expected = array(3 => 4);
+		$this->assertEquals($expected, \Mufuphlex\Util\ArrayUtil::intersect($a, $b, $c));
+	}
+
+	public function testPerformanceUnique()
+	{
+		$max = 100000;
+		$arr = range(1,$max,3);
+		$arr2 = range(1,$max,2);
+		$arr = array_merge($arr,$arr2);
+
+		$time = -microtime(true);
+		$res1 = array_unique($arr);
+		$time += microtime(true);
+		echo "\nstd array_unique:\t".count($res1)." in ".$time;
+
+		$time = -microtime(true);
+		$res2 = array();
+		foreach($arr as $key=>$val) {
+			$res2[$val] = true;
+		}
+		$res2 = array_keys($res2);
+		$time += microtime(true);
+		echo "\nvia std array_keys:\t".count($res2)." in ".$time;
+
+		$time = -microtime(true);
+		$res3 = \Mufuphlex\Util\ArrayUtil::unique($arr);
+		$time += microtime(true);
+		echo "\nmufuphlex unique:\t".count($res3)." in ".$time;
+
+		$time = -microtime(true);
+		$res4 = \Mufuphlex\Util\ArrayUtil::unique($arr, true);
+		$time += microtime(true);
+		echo "\npreserving keys:\t".count($res4)." in ".$time;
+	}
+
+	public function testPerformanceIntersect()
+	{
+		$max = 100000;
+		$arr = range(1,$max,3);
+		$arr2 = range(1,$max,2);
+
+		$time = -microtime(true);
+		$res1 = array_intersect($arr, $arr2);
+		$time += microtime(true);
+		echo "\nstd array_intersect:\t".count($res1)." in ".$time;
+
+		$time = -microtime(true);
+		$res2 = \Mufuphlex\Util\ArrayUtil::intersect($arr, $arr2);
+		$time += microtime(true);
+		echo "\nmufuphlex intersect:\t".count($res2)." in ".$time;
+	}
+}
